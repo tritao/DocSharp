324c89127149e811c5137413c220d0c367c768fe
João Matos <ripzonetriton@gmail.com>
2014-01-31 23:48:49 +0000

    Added options class.

---
+++ DocSharp/Options.cs
@@ -0,0 +1,27 @@
+﻿using DocSharp.Generators;
+
+namespace DocSharp
+{
+    public class Options
+    {
+        public Options()
+        {
+            Project = new Project();
+        }
+
+        public Project Project;
+
+        // General options
+        public bool Verbose;
+        public bool ShowHelpText;
+        public bool OutputDebug;
+
+        // Parser options
+        public bool IgnoreParseErrors;
+
+        // Generator options
+        public GeneratorKind Generator;
+        public string OutputNamespace;
+        public string OutputDir;
+    }
+}
