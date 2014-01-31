6ec2d2aaf7d994e3e10fb669183eeb6322fb180f
João Matos <ripzonetriton@gmail.com>
2014-01-31 23:46:41 +0000

    Added ColorCode-based code formatter.

---
+++ DocSharp/Generators/Formatter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using ColorCode;
+using ColorCode.Formatting;
+using ColorCode.Parsing;
+
+namespace CppSharp
+{
+    public class ColorCodeFormatter : IFormatter
+    {
+        private readonly HtmlFormatter formatter;
+
+        public ColorCodeFormatter()
+        {
+            formatter = new HtmlFormatter();
+        }
+
+        public void Write(string parsedSourceCode,
+                          IList<Scope> scopes,
+                          IStyleSheet styleSheet,
+                          TextWriter textWriter)
+        {
+            formatter.Write(parsedSourceCode, scopes, styleSheet, textWriter);
+        }
+
+        public void WriteFooter(IStyleSheet styleSheet,
+                                ILanguage language,
+                                TextWriter textWriter)
+        {
+        }
+
+        public void WriteHeader(IStyleSheet styleSheet,
+                                ILanguage language,
+                                TextWriter textWriter)
+        {
+        }
+    }
+}
