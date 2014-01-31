6ace7052deb0760dec187002cf945d109bb8241d
João Matos <ripzonetriton@gmail.com>
2014-01-31 23:42:01 +0000

    Added template interface.

---
+++ DocSharp/Templates/ITemplate.cs
@@ -0,0 +1,16 @@
+﻿using DocSharp.Generators;
+
+namespace DocSharp.Templates
+{
+    public interface ITemplate
+    {
+        #region HTML generator
+
+        void HTMLHead(HTMLTextGenerator generator, string title);
+        void HTMLFooter(HTMLTextGenerator generator);
+
+        void Process(Options options);
+
+        #endregion
+    }
+}
